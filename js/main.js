0a9699081252580224c9426d70576d4ff630c49f
Andrei <andrei40pud@mail.ru>
2019-12-23 05:07:45 +0600

    feat lesson 24 Form validation

--- js/main.js
+++ js/main.js
@@ -31,5 +31,40 @@ $(document).ready(function() {
   bullets.css("left", prev.width() + 20)
 
   new WOW().init();
-  
+
+  //Валидация формы
+  $('modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // сообщения
+      messages: {
+        userName: { 
+          required: "Имя обязательно",
+          minlength: "Слишком короткое имя",
+          maxlength: "Имя не должно превышать 15 символов"
+        },
+        userPhone: "Телефон обязателен",
+        userEmail: {
+          required: "Обязательно укажите email",
+          email: "Введите в формате name@domain.com"
+        }
+      }
+  });
+
+  // маска для телефона
+
+  $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+
 });
